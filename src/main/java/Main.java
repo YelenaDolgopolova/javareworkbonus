4a1dc12299a0fcbe8156a72a9fbf6e61a4a3b201
YelenaDolgopolova <dolgopolova-lena.95@yandex.ru>
2021-06-29 22:57:27 +0500

    change file

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -2,13 +2,13 @@ public class Main {
     public static void main(String[] args) {
 
         int balance1 = 2000;
-        int sumReplenishment = 1400;
+        int sumReplenishment = 100;
         int bonus = 0;
 
         if (sumReplenishment > 1000) {
-            bonus = balance1 + sumReplenishment/ 100;
+            bonus = sumReplenishment/ 100;
         } else {
-            bonus = sumReplenishment;
+            bonus = 0;
         }
 
         System.out.println(bonus);
